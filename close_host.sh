5b8036a1a0692ffa84dcb5362be782de00a494d0
Emrullah Taştan <emrullah69@gmail.com>
2019-12-31 17:22:49 +0300

    Add files via upload

---
+++ close_host.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# My first script
+sh source.sh remove host
+notify-send "close host to etc file"
---
+++ open_host.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# My first script
+sh source.sh add host
+notify-send "add host to etc file"
